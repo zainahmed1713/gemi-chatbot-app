c2f392817b564be495ee59c63e88f774d3368d30
Zain Ahmed <zainahmed1713@gmail.com>
2024-06-08 22:59:14 +0500

    Fix: Env not initializing in Production

--- src/components/PromptInput.jsx
+++ src/components/PromptInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { process } from "dotenv";
+require('dotenv').config();
 import { ChatContext } from "../contexts/ChatContext";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
